70d45d2b7237ba1597c47f32956f2e9f1c202f93
EikichiMizuta <akrys12152@gmail.com>
2021-10-03 20:44:49 +0900

    ゼミでクイズを作りました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,15 +1,30 @@
-const FizzBuzz = function(number) {
+const coution = "1~9999までの数字"
+console.log(coution)
+
+// 引数 number を受け取る関数
+const genkiFunction = function(number) {
+  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す//
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0 && n % 5 !== 0) {
-      console.log("Fizz")
+    //x or yに何も入っていないときに元気じゃない数字を表示する//
+    let x = null
+    let y = null
+
+    //１の位//
+    if (n % 10 === 3) {
+      console.log(n + "!!!!!!!")
+      let x = "a"
     }
-    if (n % 5 === 0 && n % 3 !== 0) {
-      console.log("Buzz")
+    //１０の位//
+    if (((n - (n % 10)) / 10) % 10 === 3 && x !== "a") {
+      console.log(n + "!!!!!!!")
+      let y = "b"
     }
-    if (n % 3 === 0 && n % 5 === 0) {
-      console.log("FizzBuzz")
+    //１００の位//
+    if (((n - (n % 100)) / 100) % 10 === 3 && (x !== "a" || y !== "b")) {
+      console.log(n + "!!!!!!!")
     }
-    if (n % 3 !== 0 && n % 5 !== 0) {
+    //元気じゃない数字の表示//
+    if (x === null && y === null) {
       console.log(n)
     }
   }
